2469027a63e66d9a70d5ca7ee7d680a6db8f8e26
xackerboydili <isoqovdilshod47@gmail.com>
2022-10-30 20:31:45 +0500

    tasks

---
+++ index.js
@@ -0,0 +1,279 @@
+"1- chi masala"
+
+// function isSameNum(num1, num2) {
+	
+// if(num1 === num2){
+//     return true
+// }
+
+// else{
+//     return false
+// }
+// }
+
+// console.log(isSameNum(2, "2"))
+
+ "2-masala,"
+
+//  function profitableGamble(prob, prize, pay) {
+	
+// if(prob * prize > pay){
+//     return true
+// }
+// else{
+//     return false
+// }
+// }
+
+// console.log(profitableGamble(0.9, 1, 2))
+
+"3- masala"
+// function areaShape(base, height, shape) {
+// 	let area;
+// 	return	shape==='triangle'? area=0.5*base*height:shape==="parallelogram"? area=base*height: area=false;
+// }
+// console.log(areaShape(5, 7, 'salom'))
+
+
+// function makesTen(a, b) {
+
+//     if(a == 10){
+//         return true
+//     }
+//     else if(b == 10) {
+//         return true
+//     }
+//     else if(a + b == 10){
+//         return true
+//     }
+//     else{
+//         return false
+//     }
+// }
+
+// console.log(makesTen(5, 10))
+
+"4-chi nasala"
+
+// function divisible(num) {
+    
+// 	if(num % 100 == 0 ){
+//         return true
+//     }
+//     else{
+//         return false
+//     }
+// }
+// console.log(divisible(99))
+
+"5-chi masala"
+
+// function salomlashish(name){
+    
+//     if(name == "Dilshod"){
+
+//     return "malades " + "Dilshod"
+//     }
+//     else {
+//         return "salom, " + name
+//     }
+// }
+
+// console.log(salomlashish("Dilshod"))
+    
+"6-chi masala"
+
+// function shouldServeDrinks(age, onBreak) {
+// 	if(age >= 18, age < 29){
+//         false
+//         return true
+//     }
+
+//     else if(age = false ){
+//         return true
+//     }
+
+//     else if(age){
+        
+//         return true
+//     }
+//     else if(onBreak = true){
+       
+//         return false
+//     }
+//     else {
+//         return false
+//     }
+    
+// }
+
+// console.log(shouldServeDrinks(30, true ))
+
+"7-chi masala"
+
+// function yeah_nope(bool) {
+	
+//    return bool ? "yeah" : "nope"
+// }
+// console.log(yeah_nope(true))
+
+"8-chi masala"
+
+// function tenglashtrish(x){
+
+//     if(x > 0){
+//         return "positive"
+//     }
+//     else if(x < 0){
+//         return "negative"
+//     }
+
+//     else {
+//         return true
+//     }
+// }
+// console.log(tenglashtrish(0))
+
+"9-chi masala"
+
+// function oddOrEven(s) {
+// 	if(s.length % 2 == 0){
+//         return true
+//     }
+//     else {
+//         return false
+//     }
+// }
+// console.log(oddOrEven("campu"))
+
+
+
+
+
+// function oddOrEven(s){
+
+//     return s.length % 2 == 0 ? true : false
+
+// }
+// console.log(oddOrEven("somer"))
+
+"10- chi masala"
+
+
+// function leapYear(year) {
+// 	return year % 4 === 0 ? true : false
+// }
+// console.log(leapYear(2022))
+
+"11-masala"
+
+// function isEvenOrOdd(num) {
+// 	return num % 2 == 0 ? "even" : "odd"
+// }
+
+
+// console.log(isEvenOrOdd(20))
+
+"12-chi masala"
+
+// function flipBool(b){
+
+// 	return b == true ? 0 : b == false ? 1 : b > 0 ? true : 1
+
+// }
+
+// console.log(flipBool(8))
+
+
+
+
+
+// function flipBool(b) {
+
+
+// 	if(b == true ){
+// 		return 0
+// 	}
+// 	else if(b == false){
+// 		return 1
+// 	}
+// 	else if(b > 0){
+// 		return true
+// 	}
+// 	else{
+// 		return 1
+// 	}
+// }
+
+// console.log(flipBool(0))
+
+"13-chi masala"
+
+// function word(s){
+// 	return s === "one" ? 1 : s === "two" ? 2 : s === "three" ? 3 : s === "four" ? 4 : s === "five" ? 5 : "enter a number from 1 to 5"
+// }
+
+// console.log(word("six"))
+// function word(s) {
+// 	if(s === "one"){
+// 		return 1
+// 	}
+// 	else if(s === "two"){
+// 		return 2
+// 	}
+// 	else if(s === "three"){
+// 		return 3
+// 	}
+// 	else if(s === "four"){
+// 		return 4
+// 	}
+// 	else if(s === "five"){
+// 		return 5
+// 	}
+// 	else if(s === "six"){
+// 		return 6
+// 	}
+// 	else if(s === "seven"){
+// 		return 7
+// 	}
+// 	else if(s === "eight"){
+// 		return 8
+// 	}
+// 	else if(s === "nine"){
+// 		return 9
+// 	}
+// 	else if(s === "ten"){
+// 		return 10
+// 	}
+// 	else if(s === "zero"){
+// 		return 0
+// 	}
+// }
+
+// console.log(word("five"))
+
+"14-chi masala"
+
+// function parity(n){
+// 	return n % 2 == 0 ? "even" : "odd"
+// }
+
+// console.log(parity(16))
+
+"15-chi masala"
+
+// function isLastCharacterN(word) {
+// 	return word.endsWith("n") ? true : false
+// }
+
+// console.log(isLastCharacterN("Annna"))
+
+
+
+// function boolToString(flag) {
+	
+// 	return flag == true ? "true" : "false"
+
+// }
+
+// console.log(boolToString(false))
